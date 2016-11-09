216b74f87b7a8e6474f66f16f4980c8fa654a578
Afrin Badhan <mbadhan@gs690-sailor9.local>
2016-11-08 20:32:57 -0500

    Cleaned up DENSTY and added error checks for mixing ratios going into WT computation. Also added logic for computing WT based on size of planet (to distinguish giants from terrestrial) to DENSTY only.

--- PHOTOCHEM/SUBROUTINES/Densty.f
+++ PHOTOCHEM/SUBROUTINES/Densty.f
@@ -12,10 +12,61 @@ C   ING HYDROSTATIC EQUILIBRIUM
       G0 = G
       RGAS = 8.3143E7
       BK = 1.38054E-16
-!      R0 = 6.371E8  (now set in PLANET.dat
-      FT = FO2 + FCO2 + FAR
-      WT = FO2*32. + FCO2*44. + (1.-FT)*28. + FAR*40.   !assuming O2,CO2,N2 and Ar are the main players and pN2=1-pO2+pCO2+pAr
-      !this is bad because CO and O can be important up high.  (but maybe this doesn't really matter)
+
+c-mab: Using radius to distinguish terrestrial from giants here
+      IF (R0.LT.(5E9)) THEN 
+c-mab: giving a sub-saturn radius value as the marker here...
+        FT = FO2 + FCO2 + FAR
+c-mab: For error checking
+        if(FO2.eq.0.)print*,"Warning: Check FO2 value, as FO2 =",FO2
+        if(FCO2.eq.0.)print*,"Warning: Check FCO2 value, as FCO2 =",FCO2
+        if(FAR.eq.0.)print*,"Warning: Check FAR value, as FAR =",FAR
+
+        if(FO2.lt.0.OR.FCO2.lt.0.OR.FAR.lt.0) then
+         print*,"Mixing ratios should not be negative!"
+         print*,"FT,FO2,FCO2,FAR = ",FT,FO2,FCO2,FAR
+         print*,"Aborting..."
+         stop
+        endif
+
+
+        WT = FO2*32. + FCO2*44. + (1.-FT)*28. + FAR*40.   !assuming O2,CO2,N2 and Ar are the main players and pN2=1-pO2+pCO2+pAr
+
+        print*, "Calculated WT for O2-N2 dominated atmosphere..."
+C this is bad because CO and O can be important up high.
+C (but maybe this doesn't really matter)
+      ELSE
+        FH2  = (1.0-FHE-FH2O-FCO-FCO2-FCH4) !assumed major species
+c-mab: For error checking
+        if(FAR.ne.0.)then
+         print*,"FAR =",FAR
+         print*,"Warning: FAR should NOT exist in this template."
+        endif
+        if(FHE.eq.0.)print*,"Warning: Check FHE value, as FHE =",FHE
+        if(FH2O.eq.0.)print*,"Warning: Check FH2O value, as FH2O =",FH2O
+        if(FCO.eq.0.)print*,"Warning: Check FCO value, as FCO =",FCO
+        if(FCO2.eq.0.)print*,"Warning: Check FCO2 value, as FCO2 =",FCO2
+        if(FCH4.eq.0.)print*,"Warning: Check FCH4 value, as FCH4 =",FCH4
+
+        if(FHE.lt.0.OR.FH2O.lt.0.OR.FCO.lt.0
+     .             .OR.FCO2.lt.0.OR.FCH4.lt.0) then
+         print*,"Mixing ratios should not be negative!"
+         print*,"FH2,FHE,FH2O,FCO,FCO2,FCH4 =",FH2,FHE,FH2O,FCO,FCO2,FCH4
+         print*,"Aborting..."
+         stop
+        endif
+C-mab: Uncomment below for debugging
+C        print*,"FH2,FHE,FCO2,FCO,FH2O,FH,FOH,FCH4",
+C     .             FH2,FHE,FCO2,FCO,FH2O,FH,FOH,FCH4
+ 
+        WT = FH2*2.0 + FHE +  FCO*28.0 + FH2O*18.0 + FH + FCH4*16.0 
+     .             + FCO2*44.0 
+c=mab: Review above expression prior to release
+
+        print*, "Calculated WT for He/H2 dominated atmosphere..."
+      ENDIF
+      
+      	print*,"Molecular weight of atmosphere = ",WT
 
       ROVERM = RGAS/WT
 
