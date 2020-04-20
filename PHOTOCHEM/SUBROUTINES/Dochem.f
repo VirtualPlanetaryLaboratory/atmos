a74f088384e0eef5c5ae86edde0c0cfa9d5bf1b1
Sandra Bastelberger <sbastelb@gs69-sbastelb.fios-router.home>
2020-04-20 18:59:23 -0400

    Corrects typo in S8 condensation

--- PHOTOCHEM/SUBROUTINES/Dochem.f
+++ PHOTOCHEM/SUBROUTINES/Dochem.f
@@ -328,7 +328,7 @@ C    !what follows is not in Jim's code
 
 C   S8 CONDENSATION (this is needed if we every want to deal with 'hot air' - s8 stays in the vapor phase       
 
-      if (LS8.neq.0) then
+      if (LS8.ne.0) then
        LL=LS8
        LLA=LS8AER
 
